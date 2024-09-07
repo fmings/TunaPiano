7d49bc9f6d6bae0d10c4d74d6cf8b865f29d5b4d
fmings <mingsfelicia@gmail.com>
2024-09-07 14:08:54 -0500

    completed genre APIs

--- API/GenreAPI.cs
+++ API/GenreAPI.cs
@@ -1,10 +1,67 @@
-﻿namespace TunaPiano.API
+﻿using Microsoft.EntityFrameworkCore;
+using TunaPiano.Models;
+
+namespace TunaPiano.API
 {
     public class GenreAPI
     {
         public static void Map(WebApplication app)
         {
+            // CREATE A GENRE
+            app.MapPost("/genres", (TunaPianaDBContext db, Genre genre) =>
+            {
+                db.Genres.Add(genre);
+                db.SaveChanges();
+                return Results.Created($"/api/genres/{genre.Id}", genre);
+            });
 
+            // DELETE A GENRE
+            app.MapDelete("/genres/{id}", (TunaPianaDBContext db, int id) =>
+            {
+                Genre genre = db.Genres.FirstOrDefault(g => g.Id == id);
+                if (genre == null)
+                {
+                    return Results.NotFound();
+                }
+                db.Genres.Remove(genre);
+                db.SaveChanges();
+                return Results.NoContent();
+            });
+
+            // UPDATE A GENRE
+            app.MapPut("/genres/{id}", (TunaPianaDBContext db, int id, Genre genre) =>
+            {
+                Genre genreToUpdate = db.Genres.SingleOrDefault(g => g.Id == id);
+                if (genreToUpdate == null)
+                {
+                    return Results.NotFound();
+                }
+                genreToUpdate.Description = genre.Description;
+
+                db.SaveChanges();
+                return Results.Ok(genreToUpdate);
+            });
+
+            // GET ALL GENRES
+            app.MapGet("/genres", (TunaPianaDBContext db) =>
+            {
+                return db.Genres.ToList();
+            });
+
+            // GET GENRE BY ID WITH ASSOCIATED SONGS
+            app.MapGet("/genres/{id}", async (TunaPianaDBContext db, int id) =>
+            {
+                Genre genre = await db.Genres
+                .Include(g => g.SongGenres)
+                    .ThenInclude(g => g.Song)
+                .FirstOrDefaultAsync(g => g.Id == id);
+
+                if (genre == null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(genre);
+            });
         }
     }
 }
--- Models/Genre.cs
+++ Models/Genre.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Description { get; set; }
-        public List<Song> Songs { get; set; }
+        public List<SongGenre> SongGenres { get; set; }
 
     } 
 }
