b8fe65e56e190f2819499698be9154496b06bae2
fmings <mingsfelicia@gmail.com>
2024-09-07 12:32:50 -0500

    completed Artists APIs

--- API/ArtistAPI.cs
+++ API/ArtistAPI.cs
@@ -1,10 +1,68 @@
-﻿namespace TunaPiano.API
+﻿using Microsoft.EntityFrameworkCore;
+using TunaPiano.Models;
+
+namespace TunaPiano.API
 {
     public class ArtistAPI
     {
         public static void Map(WebApplication app)
         {
+            // CREATE AN ARTIST
+            app.MapPost("/artists", (TunaPianaDBContext db, Artist artist) =>
+            {
+                db.Artists.Add(artist);
+                db.SaveChanges();
+                return Results.Created($"/api/artist/{artist.Id}", artist);
+            });
 
+            // DELETE AN ARTIST
+            app.MapDelete("/artists/{id}", (TunaPianaDBContext db, int id) =>
+            {
+                Artist artist = db.Artists.FirstOrDefault(a => a.Id == id);
+                if (artist == null)
+                {
+                    return Results.NotFound();
+                }
+                db.Artists.Remove(artist);
+                db.SaveChanges();
+                return Results.NoContent();
+            });
+
+            // UPDATE AN ARTIST
+            app.MapPut("/artists/{id}", (TunaPianaDBContext db, int id, Artist artist) =>
+            {
+                Artist artistToUpdate = db.Artists.SingleOrDefault(a => a.Id == id);
+                if (artistToUpdate == null)
+                {
+                    return Results.NotFound();
+                }
+                artistToUpdate.Name = artist.Name;
+                artistToUpdate.Age = artist.Age;
+                artistToUpdate.Bio = artist.Bio;
+
+                db.SaveChanges();
+                return Results.Ok(artistToUpdate);
+            });
+
+            // GET ALL ARTISTS
+            app.MapGet("/artists", (TunaPianaDBContext db) =>
+            {
+                return db.Artists.ToList();
+            });
+
+            // GET ARTIST BY ID WITH ASSOCIATED SONGS
+            app.MapGet("/artist/{id}", async (TunaPianaDBContext db, int id) =>
+            {
+                Artist artist = await db.Artists
+                .Include(a => a.Songs)
+                .FirstOrDefaultAsync(s => s.Id == id);
+
+                if (artist == null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(artist);
+            });
         }
     }
 }
--- Migrations/20240907172947_AddedSongsToArtistModel.Designer.cs
+++ Migrations/20240907172947_AddedSongsToArtistModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TunaPiano;
@@ -11,9 +12,11 @@ using TunaPiano;
 namespace TunaPiano.Migrations
 {
     [DbContext(typeof(TunaPianaDBContext))]
-    partial class TunaPianaDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240907172947_AddedSongsToArtistModel")]
+    partial class AddedSongsToArtistModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
